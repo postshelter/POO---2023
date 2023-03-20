8c46b1049ba39908220f79fb93cf71b1edbdd2b3
Victor Ribeiro <127801745+postshelter@users.noreply.github.com>
2023-03-20 15:32:47 -0300

    Add files via upload

---
+++ Exercício 4.py
@@ -0,0 +1,13 @@
+class Musica:
+    def __init__(self, nome, artistabanda, genero, ano, duracao):
+        self.nome = nome
+        self.artistabanda = artistabanda
+        self.genero = genero
+        self.ano = ano
+        self.duracao = duracao
+
+lt = Musica('Lagtrain', 'Inabakumori', 'Vocaloid','2020','4:11')
+ps = Musica('Post Shelter', 'Inabakumori', 'Vocaloid','2022','3:20')
+utt = Musica('UNDER THE TREE', 'SiM', 'J-Rock', '2023', '2:20')
+rmb = Musica ('The Rumbling', 'SiM', 'J-Rock', '2022', '3:41')
+gbs = Musica('goosebumps', 'Travis Scott', 'Rap', '2016', '4:04')
