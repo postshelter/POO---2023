ec24050216d55c48186fde2d78ce6f7219e87d19
Victor Ribeiro <127801745+postshelter@users.noreply.github.com>
2023-03-21 16:21:24 -0300

    Update Exercício 4.py

--- Lista 1/Exercício 4.py
+++ Lista 1/Exercício 4.py
@@ -6,8 +6,33 @@ class Musica:
         self.ano = ano
         self.duracao = duracao
 
+    def play(self):
+        print('Tocando agora {} por {}.'.format(self.nome,self.artistabanda))
+
+    def informacoes(self):
+        print('''Título: {} 
+Artista/Banda: {}
+Gênero: {}
+Ano: {}
+Duração: {}.'''.format(self.nome, self.artistabanda, self.genero, self.ano, self.duracao))
+
 lt = Musica('Lagtrain', 'Inabakumori', 'Vocaloid','2020','4:11')
 ps = Musica('Post Shelter', 'Inabakumori', 'Vocaloid','2022','3:20')
 utt = Musica('UNDER THE TREE', 'SiM', 'J-Rock', '2023', '2:20')
 rmb = Musica ('The Rumbling', 'SiM', 'J-Rock', '2022', '3:41')
 gbs = Musica('goosebumps', 'Travis Scott', 'Rap', '2016', '4:04')
+
+lt.play()
+lt.informacoes()
+
+ps.play()
+ps.informacoes()
+
+utt.play()
+utt.informacoes()
+
+rmb.play()
+rmb.informacoes()
+
+gbs.play()
+gbs.informacoes()
